f478526cbe33a6409fc2eab353b4e0c8780466dc
Vlad <Vladroon22@mail.ru>
2024-04-11 11:43:50 +0300

    db changes

--- migrations/000001_in.up.sql
+++ migrations/000001_in.up.sql
@@ -1,7 +1,7 @@
 -- Active: 1712649823557@@127.0.0.1@5430@rest_api_db@public
 CREATE TABLE users (
-    id INT PRIMARY KEY not null,
-    username VARCHAR(255) not null,
-    email VARCHAR(255) not null,
-    encrypt_password VARCHAR(255) not null
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    encrypt_password VARCHAR(255) NOT NULL
 );
