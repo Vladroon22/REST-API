6279a100677a17b00af6d10d99098ef9728430d9
Vlad <Vladroon22@mail.ru>
2024-04-15 15:52:14 +0300

    Update restserver.go

--- internal/server/restserver.go
+++ internal/server/restserver.go
@@ -43,31 +43,3 @@ func (s *Server) Run() {
 	s.logger.Infoln("Server is listening -->")
 	s.logger.Fatalln(s.server.ListenAndServe())
 }
-
-/*
-func shutdown(s *Server, ctx context.Context) {
-	var wait time.Duration
-	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
-	flag.Parse()
-
-	c := make(chan os.Signal, 1)
-	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
-	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
-	signal.Notify(c, os.Interrupt, syscall.SIGINT)
-
-	// Block until we receive our signal.
-	<-c
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), wait)
-	defer cancel()
-	// Doesn't block if no connections, but will otherwise wait
-	// until the timeout deadline.
-	s.HttpServer.Shutdown(ctx)
-	// Optionally, you could run srv.Shutdown in a goroutine and block on
-	// <-ctx.Done() if your application should wait for other services
-	// to finalize based on context cancellation.
-	s.Logger.Infoln("Graceful shutting down")
-	os.Exit(0)
-}
-*/
