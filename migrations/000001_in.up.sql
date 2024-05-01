0ebb77be2997a58018631d42e8a48b74055e6129
Vlad <Vladroon22@mail.ru>
2024-05-01 18:45:23 +0300

    Update 000001_in.up.sql

--- migrations/000001_in.up.sql
+++ migrations/000001_in.up.sql
@@ -1,6 +1,6 @@
 -- Active: 1712649823557@@127.0.0.1@5430@rest_api_db@public
 CREATE TABLE users (
-    id SERIAL UNSIGNED AUTO_INCREMENT,
+    id SERIAL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     encrypt_password VARCHAR(255) NOT NULL
