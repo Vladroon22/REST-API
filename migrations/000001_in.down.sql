409f65b44008ef68415f8d019ac308cf4f2458d0
Vlad <Vladroon22@mail.ru>
2024-05-02 23:39:04 +0300

    migration-changes

--- migrations/000001_in.down.sql
+++ migrations/000001_in.down.sql
@@ -1,2 +1,2 @@
 -- Active: 1712649823557@@127.0.0.1@5430@rest_api_db@public
-DROP TABLE IF EXISTS users
+DROP TABLE IF EXISTS clients
--- migrations/000001_in.up.sql
+++ migrations/000001_in.up.sql
@@ -1,5 +1,5 @@
 -- Active: 1712649823557@@127.0.0.1@5430@rest_api_db@public
-CREATE TABLE users (
+CREATE TABLE clients (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
